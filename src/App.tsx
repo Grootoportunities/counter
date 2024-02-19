d65dbb291f899bc86486774e661bc27652f62ed5
Grootoportunities <tapir1337@mail.ru>
2024-02-19 17:50:00 +0300

    Refactoring

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
-import "./App.css";
-import { Counter } from "./components/counter/Counter";
-
-function App() {
-  return <Counter />;
-}
-
-export default App;
